5e9e9892d6e3cb99b97aed736bef1c84495741ce
Othodrin <123425033+Othodrin@users.noreply.github.com>
2023-01-23 18:34:45 -0500

    Update Convert.java

--- Convert.java
+++ Convert.java
@@ -4,7 +4,6 @@
 // Date: Friday, February 3, 2023
 public class Convert{
    public static double convert(String temperature, char scale){
-      // converts a temperature (value/scale) to a new scale
       double temp;
       char currentScale;
       int length = temperature.length();
@@ -20,25 +19,5 @@ public class Convert{
          }
       }
       return temp;
-   }
-
-   public static void main(String[] args){
-      System.out.println("Testing Convert.convert");
-      String in = "10.0C";
-      char scale = 'F';
-      double expect = 50.0;
-      double out = Convert.convert(in, scale);
-      System.out.println("convert(\"" + in + "\",\'" + scale + "\')");
-      System.out.println("...expect : " + expect);
-      System.out.println("...actual : " + out);
-      
-      in = "10.0C";
-      scale = 'C';
-      expect = 10.0;
-      out = Convert.convert(in, scale);
-      System.out.println("convert(\"" + in + "\",\'" + scale + "\')");
-      System.out.println("...expect : " + expect);
-      System.out.println("...actual : " + out);
-      
    }
 }
